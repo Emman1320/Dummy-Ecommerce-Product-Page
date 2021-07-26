1a837064c50438e88e7066b14187f5792a0f049a
Emman1320 <81506072+Emman1320@users.noreply.github.com>
2021-07-26 19:01:34 +0530

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,18 +1,19 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 });
-function scrollOver(scrollMultplier) {
+function scrollOver(scrollMultiplier) {
   var scrollLength;
   if (window.innerWidth <= 1100) {
     scrollLength = 719;
   } else if (window.innerWidth <= 1368) {
     scrollLength = 580;
   } else {
-    scrollByLength = 590;
+    scrollLength = 590;
   }
 
-  var currentPostion = document.getElementById("scroll").scrollLeft;
-  var scrollByLength = scrollLength * scrollMultplier - currentPostion;
+  var currentPosition = document.getElementById("scroll").scrollLeft;
+  var scrollByLength = scrollLength * scrollMultiplier - currentPosition;
+  console.log(scrollLength);
   document.getElementById("scroll").scrollBy(scrollByLength, 0);
   console.log(document.getElementById("scroll").scrollLeft);
 }
